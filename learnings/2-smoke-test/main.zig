842d792aba9c7d2a757530ac1326cc949f8a5697
Oliver Wilkins <okwilkins@googlemail.com>
2024-10-30 23:38:57 +0000

    feat(smoke-test): Init server + Dockerfile

---
+++ learnings/2-smoke-test/main.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+const net = std.net;
+const posix = std.posix;
+
+pub fn main() !void {
+    const addr = try net.Address.parseIp("127.0.0.1", 1337);
+    const socket_type = posix.SOCK.STREAM;
+    const protocol = posix.IPPROTO.TCP;
+    const listener = try posix.socket(addr.any.family, socket_type, protocol);
+    defer posix.close(listener);
+
+    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try posix.bind(listener, &addr.any, addr.getOsSockLen());
+    try posix.listen(listener, 128);
+    var buf: [128]u8 = undefined;
+    std.debug.print("server starting...", .{});
+
+    while (true) {
+        var client_address: net.Address = undefined;
+        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+        const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
+            std.debug.print("error accept: {}\n", .{err});
+            continue;
+        };
+        defer posix.close(socket);
+
+        std.debug.print("{} connected\n", .{client_address});
+
+        const read = posix.read(socket, &buf) catch |err| {
+            std.debug.print("error reading: {}\n", .{err});
+            continue;
+        };
+
+        if (read == 0) {
+            continue;
+        }
+
+        write(socket, buf[0..read]) catch |err| {
+            std.debug.print("error writing: {}\n", .{err});
+        };
+    }
+}
+
+fn write(socket: posix.socket_t, msg: []const u8) !void {
+    var pos: usize = 0;
+    while (pos < msg.len) {
+        const written = try posix.write(socket, msg[pos..]);
+        if (written == 0) {
+            return error.Closed;
+        }
+        pos += written;
+    }
+}
