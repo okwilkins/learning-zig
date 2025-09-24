bf834f5b654b378d6d3ec60744127a0592b48665
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-24 20:46:37 +0100

    feat: Completed interface and memory allocation system for doubly linked list
    fix

--- data-structures/5-linked-list/main.zig
+++ data-structures/5-linked-list/main.zig
@@ -1,16 +1,66 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 fn Node(comptime T: type) type {
     return struct {
-        value: ?T,
-        next: ?*Node = null,
-        prev: ?*Node = null,
+        value: T,
+        next: ?*Node(T) = null,
+        prev: ?*Node(T) = null,
     };
 }
 
-var LinkedList = struct {};
+fn LinkedList(comptime T: type) type {
+    return struct {
+        const Self = @This();
 
-fn main() void {
-    const allocator = std.heap.GeneralPurposeAllocator(.{});
-    LinkedList(isize).init(allocator);
+        head: ?*Node(T) = null,
+        tail: ?*Node(T) = null,
+        allocator: Allocator,
+
+        fn init(allocator: Allocator) Self {
+            return Self{ .allocator = allocator };
+        }
+
+        fn deinit(self: *Self) void {
+            var current = self.head;
+
+            while (current) |node| {
+                const next = node.next;
+                self.allocator.destroy(node);
+                current = next;
+            }
+
+            self.* = undefined;
+        }
+
+        fn append(self: *Self, value: T) !void {
+            const new_node = try self.allocator.create(Node(T));
+            new_node.* = .{ .value = value };
+
+            if (self.tail) |tail_node| {
+                new_node.prev = tail_node;
+                tail_node.next = new_node;
+            } else {
+                self.head = new_node;
+            }
+
+            self.tail = new_node;
+        }
+
+        fn first() *Node {}
+    };
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) @panic("Memory leaked!");
+    }
+
+    var linked_list = LinkedList(?usize).init(allocator);
+    defer linked_list.deinit();
+
+    try linked_list.append(null);
 }
