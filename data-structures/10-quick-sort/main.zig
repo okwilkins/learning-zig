8e657e4837feee13d97663f2eda613692dac2f6c
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-15 22:00:47 +0100

    feat: Virtual table impl

--- data-structures/10-quick-sort/main.zig
+++ data-structures/10-quick-sort/main.zig
@@ -1,30 +1,83 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-fn quickSort(comptime T: type, items: []T) void {
-    if (items.len <= 1) return;
+const QuickSortStrat = enum {
+    firstElement,
+    lastElement,
+    middleElement,
+    randomElement,
+    medianOfThree,
+};
 
-    const pivot = items[items.len - 1];
-    std.debug.print("Pivot: {d}\n", .{pivot});
-    var i: usize = 0;
+fn Pivoter(comptime T: type) type {
+    return struct {
+        const Self = @This();
 
-    for (items[0..items.len], 0..) |item, j| {
-        if (item < pivot) {
-            std.mem.swap(T, &items[i], &items[j]);
-            i += 1;
+        ptr: *anyopaque,
+        vtable: *const VTable,
+
+        const VTable = struct {
+            pivot: *const fn (*anyopaque, []T) usize,
+        };
+
+        fn pivot(self: Self, items: []T) usize {
+            return self.vtable.pivot(self.ptr, items);
         }
-        std.debug.print(
-            "i: {d} | j: {d} | Array: {any}\n",
-            .{ i, j, items },
-        );
-    }
+    };
+}
 
-    std.mem.swap(T, &items[i], &items[items.len - 1]);
+fn FirstElementPivoter(comptime T: type) type {
+    return struct {
+        const Self = @This();
 
-    quickSort(T, items[0..i]);
-    if (i + 1 < items.len) {
-        quickSort(T, items[i + 1 ..]);
-    }
+        fn getPivotIndex(self: *Self, items: []T) usize {
+            _ = self;
+            _ = items;
+            return 0;
+        }
+
+        fn getPivotIndexOpaque(ptr: *anyopaque, items: []T) usize {
+            const pivoter: *Self = @ptrCast(@alignCast(ptr));
+            return pivoter.getPivotIndex(items);
+        }
+    };
+}
+
+fn quickSorter(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        strat: QuickSortStrat,
+
+        fn init(strat: QuickSortStrat) Self {
+            return Self{ .strat = strat };
+        }
+
+        fn sort(self: *Self, items: []T) void {
+            if (items.len <= 1) return;
+
+            const pivot = items[items.len - 1];
+            std.debug.print("Pivot: {d}\n", .{pivot});
+            var i: usize = 0;
+
+            for (items[0..items.len], 0..) |item, j| {
+                if (item < pivot) {
+                    std.mem.swap(T, &items[i], &items[j]);
+                    i += 1;
+                }
+                std.debug.print(
+                    "i: {d} | j: {d} | Array: {any}\n",
+                    .{ i, j, items },
+                );
+            }
+
+            std.mem.swap(T, &items[i], &items[items.len - 1]);
+
+            self.sort(items[0..i]);
+            if (i + 1 < items.len) {
+                self.sort(items[i + 1 ..]);
+            }
+        }
+    };
 }
 
 pub fn main() !void {
@@ -36,6 +89,7 @@ pub fn main() !void {
     }
 
     std.debug.print("Pre-sorted array: {any}\n", .{items});
-    quickSort(usize, &items);
+    var sorter = quickSorter(usize).init(QuickSortStrat.lastElement);
+    sorter.sort(&items);
     std.debug.print("Sorted array: {any}\n", .{items});
 }
